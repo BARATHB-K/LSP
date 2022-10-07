906eb663155d28e2c3ffa1c3d97632211caf37bb
BARATHB-K <92674796+BARATHB-K@users.noreply.github.com>
2022-10-07 23:22:34 +0530

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,33 +3,6 @@ import 'package:lsp/navigation/naviigation_drawer.dart';
 import 'package:lsp/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
 
-/*class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
-              print("Signed Out");
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SignInScreen()));
-            });
-          },
-        ),
-      ),
-    );
-  }
-}*/
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -53,27 +26,7 @@ class HomeScreen extends StatelessWidget {
             });
           },
         ),
-      ), /*body: Builder(builder: (context) {
-          return Center(
-            child: SizedBox(
-              height: 50,
-              width: MediaQuery.of(context).size.width - 100,
-              child: ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                ),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                icon: const Icon(Icons.open_in_new, color: Colors.white),
-                label: const Text(
-                  'Open Navigation Drawer',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          );
-        })*/
+      ),  
     );
   }
 }
